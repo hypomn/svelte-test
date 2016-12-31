5fa44de5d2ddab394d5781550082d193fda473df
Markus Nakhlah <markus@nakhlah.intern>
2016-12-31 14:30:24 +0000

    First commit

---
+++ TodoList.js
@@ -0,0 +1,227 @@
+var TodoList = (function () { 'use strict';
+
+function renderMainFragment ( root, component ) {
+	var h1 = createElement( 'h1' );
+	
+	var text = createText( root.listTitle );
+	appendNode( text, h1 );
+	var text1 = createText( "\r\n" );
+	
+	var ul = createElement( 'ul' );
+	
+	var eachBlock_anchor = createComment( "#each todoItems" );
+	appendNode( eachBlock_anchor, ul );
+	var eachBlock_value = root.todoItems;
+	var eachBlock_iterations = [];
+	
+	for ( var i = 0; i < eachBlock_value.length; i += 1 ) {
+		eachBlock_iterations[i] = renderEachBlock( root, eachBlock_value, eachBlock_value[i], i, component );
+		eachBlock_iterations[i].mount( eachBlock_anchor.parentNode, eachBlock_anchor );
+	}
+
+	return {
+		mount: function ( target, anchor ) {
+			insertNode( h1, target, anchor );
+			insertNode( text1, target, anchor );
+			insertNode( ul, target, anchor );
+		},
+		
+		update: function ( changed, root ) {
+			text.data = root.listTitle;
+			
+			var eachBlock_value = root.todoItems;
+			
+			for ( var i = 0; i < eachBlock_value.length; i += 1 ) {
+				if ( !eachBlock_iterations[i] ) {
+					eachBlock_iterations[i] = renderEachBlock( root, eachBlock_value, eachBlock_value[i], i, component );
+					eachBlock_iterations[i].mount( eachBlock_anchor.parentNode, eachBlock_anchor );
+				} else {
+					eachBlock_iterations[i].update( changed, root, eachBlock_value, eachBlock_value[i], i );
+				}
+			}
+			
+			for ( var i = eachBlock_value.length; i < eachBlock_iterations.length; i += 1 ) {
+				eachBlock_iterations[i].teardown( true );
+			}
+			
+			eachBlock_iterations.length = eachBlock_value.length;
+		},
+		
+		teardown: function ( detach ) {
+			for ( var i = 0; i < eachBlock_iterations.length; i += 1 ) {
+				eachBlock_iterations[i].teardown( false );
+			}
+			
+			if ( detach ) {
+				detachNode( h1 );
+				detachNode( text1 );
+				detachNode( ul );
+			}
+		},
+	};
+}
+
+function renderEachBlock ( root, eachBlock_value, todoItem, todoItem__index, component ) {
+	var li = createElement( 'li' );
+	
+	var text = createText( todoItem.title );
+	appendNode( text, li );
+	
+	var input = createElement( 'input' );
+	input.type = "checkbox";
+	input.checked = todoItem.done;
+	
+	appendNode( input, li );
+
+	return {
+		mount: function ( target, anchor ) {
+			insertNode( li, target, anchor );
+		},
+		
+		update: function ( changed, root, eachBlock_value, todoItem, todoItem__index ) {
+			var todoItem = eachBlock_value[todoItem__index];
+			
+			text.data = todoItem.title;
+			
+			input.checked = todoItem.done;
+		},
+		
+		teardown: function ( detach ) {
+			if ( detach ) {
+				detachNode( li );
+			}
+		},
+	};
+}
+
+function TodoList ( options ) {
+	options = options || {};
+	
+	this._state = options.data || {};
+
+	this._observers = {
+		pre: Object.create( null ),
+		post: Object.create( null )
+	};
+
+	this._handlers = Object.create( null );
+
+	this._root = options._root;
+	this._yield = options._yield;
+
+	this._fragment = renderMainFragment( this._state, this );
+	if ( options.target ) this._fragment.mount( options.target, null );
+}
+
+TodoList.prototype.get = function get( key ) {
+ 	return key ? this._state[ key ] : this._state;
+ };
+
+TodoList.prototype.fire = function fire( eventName, data ) {
+ 	var handlers = eventName in this._handlers && this._handlers[ eventName ].slice();
+ 	if ( !handlers ) return;
+ 
+ 	for ( var i = 0; i < handlers.length; i += 1 ) {
+ 		handlers[i].call( this, data );
+ 	}
+ };
+
+TodoList.prototype.observe = function observe( key, callback, options ) {
+ 	var group = ( options && options.defer ) ? this._observers.pre : this._observers.post;
+ 
+ 	( group[ key ] || ( group[ key ] = [] ) ).push( callback );
+ 
+ 	if ( !options || options.init !== false ) {
+ 		callback.__calling = true;
+ 		callback.call( this, this._state[ key ] );
+ 		callback.__calling = false;
+ 	}
+ 
+ 	return {
+ 		cancel: function () {
+ 			var index = group[ key ].indexOf( callback );
+ 			if ( ~index ) group[ key ].splice( index, 1 );
+ 		}
+ 	};
+ };
+
+TodoList.prototype.on = function on( eventName, handler ) {
+ 	var handlers = this._handlers[ eventName ] || ( this._handlers[ eventName ] = [] );
+ 	handlers.push( handler );
+ 
+ 	return {
+ 		cancel: function () {
+ 			var index = handlers.indexOf( handler );
+ 			if ( ~index ) handlers.splice( index, 1 );
+ 		}
+ 	};
+ };
+
+TodoList.prototype.set = function set ( newState ) {
+	var oldState = this._state;
+	this._state = Object.assign( {}, oldState, newState );
+	
+	dispatchObservers( this, this._observers.pre, newState, oldState );
+	if ( this._fragment ) this._fragment.update( newState, this._state );
+	dispatchObservers( this, this._observers.post, newState, oldState );
+};
+
+TodoList.prototype.teardown = function teardown ( detach ) {
+	this.fire( 'teardown' );
+
+	this._fragment.teardown( detach !== false );
+	this._fragment = null;
+
+	this._state = {};
+};
+
+function dispatchObservers( component, group, newState, oldState ) {
+	for ( var key in group ) {
+		if ( !( key in newState ) ) continue;
+
+		var newValue = newState[ key ];
+		var oldValue = oldState[ key ];
+
+		if ( newValue === oldValue && typeof newValue !== 'object' ) continue;
+
+		var callbacks = group[ key ];
+		if ( !callbacks ) continue;
+
+		for ( var i = 0; i < callbacks.length; i += 1 ) {
+			var callback = callbacks[i];
+			if ( callback.__calling ) continue;
+
+			callback.__calling = true;
+			callback.call( component, newValue, oldValue );
+			callback.__calling = false;
+		}
+	}
+}
+
+function createElement( name ) {
+	return document.createElement( name );
+}
+
+function detachNode( node ) {
+	node.parentNode.removeChild( node );
+}
+
+function insertNode( node, target, anchor ) {
+	target.insertBefore( node, anchor );
+}
+
+function createText( data ) {
+	return document.createTextNode( data );
+}
+
+function appendNode( node, target ) {
+	target.appendChild( node );
+}
+
+function createComment( data ) {
+	return document.createComment( data );
+}
+
+return TodoList;
+
+}());
